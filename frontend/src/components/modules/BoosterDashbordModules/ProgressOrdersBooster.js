e951a0696bb90da85f941585da592277612f5b34
Maamoun <grissa.maamoun@gmail.com>
2021-07-16 02:02:37 +0100

    valorantboosting v2.3

--- frontend/src/components/modules/BoosterDashbordModules/ProgressOrdersBooster.js
+++ frontend/src/components/modules/BoosterDashbordModules/ProgressOrdersBooster.js
@@ -8,6 +8,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import ForwardIcon from '@material-ui/icons/Forward';
 
 const useStyles = makeStyles({
   table: {
@@ -15,22 +16,37 @@ const useStyles = makeStyles({
   },
 });
 
-
 export default function ProgressOrdersBooster(props) {
     const classes = useStyles();
     const { orders, clients, user } = props;
+
+    const ranks = [
+        "iron",
+        "bronze",
+        "silver",
+        "gold",
+        "platinum",
+        "diamond",
+        "immortal"
+    ];
+
+    const divisions = [
+        "I",
+        "II",
+        'III',
+    ]
 
     return (
       <TableContainer component={Paper}>
         <Table className={classes.table} aria-label="simple table">
           <TableHead className="custom-thead">
             <TableRow>
-              <TableCell>Id</TableCell>
               <TableCell>Date</TableCell>
               <TableCell>User</TableCell>
               <TableCell>Boost Type</TableCell>
               <TableCell>Queue</TableCell>
               <TableCell>Server</TableCell>
+              <TableCell className="text-center">Boost</TableCell>
               <TableCell>Your Price</TableCell>
               <TableCell>Action</TableCell>
             </TableRow>
@@ -40,12 +56,30 @@ export default function ProgressOrdersBooster(props) {
               <TableRow key={order._id} 
                 className={props.selectedOrder === order._id ? 'mytablerow active' : 'mytablerow'} 
                 onClick={() => props.selectOrder(order._id)}>
-                <TableCell>{order._id.substring(order._id.length - 5)}</TableCell>
                 <TableCell>{Moment(order.createdAt).format('DD/MM/YY')}</TableCell>
                 <TableCell>{clients.find(b => b._id === order.userId)?.name}</TableCell>
-                <TableCell>{order.boostType}</TableCell>
-                <TableCell>{order.duoGame ? 'Duo Boost' : 'Solo Boost'}</TableCell>
+                <TableCell>{order.boostType.split(' ')[0]}</TableCell>
+                <TableCell>{order.duoGame ? 'Duo' : 'Solo'}</TableCell>
                 <TableCell>{order.server}</TableCell>
+                <TableCell>
+                  <span className="nowrap">
+                    {
+                      order.boostType === "Rank Boosting" ? (
+                          <span className="flex-align-center no-m j-fs">
+                              <span className="cap">{ranks[order.startRank - 1]}</span>
+                              <span>&nbsp;{divisions[order.startDivision - 1]}</span>
+                          </span>
+                      ) : (
+                          <span>{order.games}&nbsp;Games</span>
+                      )
+                    }
+                    <ForwardIcon />
+                    <span className="flex-align-center no-m j-fs">
+                        <span className="cap">{ranks[order.desiredRank - 1]}</span>
+                        <span>&nbsp;{divisions[order.desiredDivision - 1]}</span>
+                    </span>
+                  </span>
+                </TableCell>
                 <TableCell>{parseInt((order.price / 100) * user.percentage)}&nbsp;$</TableCell>
                 <TableCell><button className="paid-button blue" onClick={() => props.onTake(order._id) }>Take</button></TableCell>
               </TableRow>
